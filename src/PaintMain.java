bcc1e9aa5f2b3d605411c5775ee3132da9cc364f
yymin1022 <yymin1022@gmail.com>
2020-05-14 00:25:32 +0900

    Update Main Layout

--- src/PaintMain.java
+++ src/PaintMain.java
@@ -1,5 +1,27 @@
-public class PaintMain {
+import javax.swing.*;
+import java.awt.*;
+
+public class PaintMain extends JFrame{
+    JButton btnCircle, btnMouse, btnRectangle, btnTriangle;
+    JButton btnMove, btnRotate;
+    JPanel panelDraw, panelMain;
+
+    public PaintMain(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setSize(900, 750);
+        setTitle("Paint");
+
+        panelMain = new JPanel();
+        panelMain.setBackground(Color.GRAY);
+        panelMain.setLayout(null);
+
+        add(panelMain);
+        setVisible(true);
+    }
+
     public static void main(String[] arg0){
-        System.out.println("Hello, World!");
+        new PaintMain();
     }
 }
