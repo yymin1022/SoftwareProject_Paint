3a4e89bef96bd35c3f214eb0a6e1d0054ddfd22d
yymin1022 <yymin1022@gmail.com>
2020-05-16 21:14:26 +0900

    Update MouseMotionListener

--- src/PaintMain.java
+++ src/PaintMain.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
-import javax.swing.text.View;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
-import java.io.Serializable;
 import java.util.ArrayList;
 
 public class PaintMain extends JFrame{
+    int penStartX, penStartY, penEndX, penEndY;
+
+    Graphics graphics;
+    Graphics2D graphics2D;
+
     JButton btnCircle, btnDiamond, btnMouse, btnRectangle, btnTriangle;
     JButton btnColor, btnJPEG, btnMove, btnRotate, btnSize;
     JPanel panelDraw, panelMain;
@@ -16,6 +21,7 @@ public class PaintMain extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setLocationRelativeTo(null);
+        setResizable(false);
         setSize(510, 600);
         setTitle("Paint");
 
@@ -45,6 +51,18 @@ public class PaintMain extends JFrame{
         btnRotate.setBounds(110, 70, 90, 50);
         btnSize.setBounds(310, 70, 90, 50);
 
+        btnCircle.addActionListener(new drawButtonClickListener());
+        btnDiamond.addActionListener(new drawButtonClickListener());
+        btnMouse.addActionListener(new drawButtonClickListener());
+        btnRectangle.addActionListener(new drawButtonClickListener());
+        btnTriangle.addActionListener(new drawButtonClickListener());
+
+        btnColor.addActionListener(new buttonClickListener());
+        btnJPEG.addActionListener(new buttonClickListener());
+        btnMove.addActionListener(new buttonClickListener());
+        btnRotate.addActionListener(new buttonClickListener());
+        btnSize.addActionListener(new buttonClickListener());
+
         panelMain.add(btnCircle);
         panelMain.add(btnDiamond);
         panelMain.add(btnMouse);
@@ -64,6 +82,10 @@ public class PaintMain extends JFrame{
         add(panelMain);
         add(panelDraw);
         setVisible(true);
+
+        graphics = getGraphics();
+        graphics.setColor(Color.black);
+        graphics2D = (Graphics2D)graphics;
     }
 
     public static void main(String[] arg0){
@@ -75,12 +97,65 @@ public class PaintMain extends JFrame{
     public class drawPen implements MouseMotionListener{
         @Override
         public void mouseDragged(MouseEvent e) {
+            penEndX = e.getX();
+            penEndY = e.getY();
 
+            graphics2D.setStroke(new BasicStroke(10, BasicStroke.CAP_SQUARE,0));
+            graphics2D.drawLine(penStartX + 10, penStartY + 150, penEndX + 10, penEndY + 150);
+
+            penStartX = penEndX;
+            penStartY = penEndY;
         }
 
         @Override
         public void mouseMoved(MouseEvent e) {
 
+        }
+    }
+
+    public class drawButtonClickListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == btnCircle){
+
+            }else if(e.getSource() == btnDiamond){
+
+            }else if(e.getSource() == btnMouse){
+                panelDraw.addMouseMotionListener(new MouseMotionListener() {
+                    @Override
+                    public void mouseDragged(MouseEvent e) {
+                        penStartX = e.getX();
+                        penStartY = e.getY();
+                    }
+
+                    @Override
+                    public void mouseMoved(MouseEvent e) {
+
+                    }
+                });
+                panelDraw.addMouseMotionListener(new drawPen());
+            }else if(e.getSource() == btnRectangle){
+                
+            }else if(e.getSource() == btnTriangle){
+
+            }
+        }
+    }
+
+    public class buttonClickListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == btnColor){
+
+            }else if(e.getSource() == btnJPEG){
+
+            }else if(e.getSource() == btnMove){
+
+            }else if(e.getSource() == btnRotate){
+
+            }else if(e.getSource() == btnSize){
+
+            }
         }
     }
 }
