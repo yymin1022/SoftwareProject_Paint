52d88598a2c60e790be9235fc5c7bf9856adbee4
yymin1022 <yymin1022@gmail.com>
2020-05-16 22:14:25 +0900

    Update Shape : Circle

--- src/PaintMain.java
+++ src/PaintMain.java
@@ -1,13 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
+import java.awt.font.ShapeGraphicAttribute;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class PaintMain extends JFrame{
     int penStartX, penStartY, penEndX, penEndY;
+    int shapeType = 3;
+/*  0 : Circle
+    1 : Diamone
+    2 : Line (Mouse)
+    3 : Rectangle
+    4 : Triangle
+ */
 
     Graphics graphics;
     Graphics2D graphics2D;
@@ -51,17 +57,17 @@ public class PaintMain extends JFrame{
         btnRotate.setBounds(110, 70, 90, 50);
         btnSize.setBounds(310, 70, 90, 50);
 
-        btnCircle.addActionListener(new drawButtonClickListener());
-        btnDiamond.addActionListener(new drawButtonClickListener());
-        btnMouse.addActionListener(new drawButtonClickListener());
-        btnRectangle.addActionListener(new drawButtonClickListener());
-        btnTriangle.addActionListener(new drawButtonClickListener());
+        btnCircle.addActionListener(drawButtonClickListener);
+        btnDiamond.addActionListener(drawButtonClickListener);
+        btnMouse.addActionListener(drawButtonClickListener);
+        btnRectangle.addActionListener(drawButtonClickListener);
+        btnTriangle.addActionListener(drawButtonClickListener);
 
-        btnColor.addActionListener(new buttonClickListener());
-        btnJPEG.addActionListener(new buttonClickListener());
-        btnMove.addActionListener(new buttonClickListener());
-        btnRotate.addActionListener(new buttonClickListener());
-        btnSize.addActionListener(new buttonClickListener());
+        btnColor.addActionListener(buttonClickListener);
+        btnJPEG.addActionListener(buttonClickListener);
+        btnMove.addActionListener(buttonClickListener);
+        btnRotate.addActionListener(buttonClickListener);
+        btnSize.addActionListener(buttonClickListener);
 
         panelMain.add(btnCircle);
         panelMain.add(btnDiamond);
@@ -84,8 +90,9 @@ public class PaintMain extends JFrame{
         setVisible(true);
 
         graphics = getGraphics();
-        graphics.setColor(Color.black);
         graphics2D = (Graphics2D)graphics;
+        graphics2D.setColor(Color.black);
+        graphics2D.setStroke(new BasicStroke(5, BasicStroke.CAP_ROUND,0));
     }
 
     public static void main(String[] arg0){
@@ -94,13 +101,12 @@ public class PaintMain extends JFrame{
         new PaintMain();
     }
 
-    public class drawPen implements MouseMotionListener{
+    MouseMotionListener drawPenListener = new MouseMotionListener(){
         @Override
         public void mouseDragged(MouseEvent e) {
             penEndX = e.getX();
             penEndY = e.getY();
 
-            graphics2D.setStroke(new BasicStroke(10, BasicStroke.CAP_SQUARE,0));
             graphics2D.drawLine(penStartX + 10, penStartY + 150, penEndX + 10, penEndY + 150);
 
             penStartX = penEndX;
@@ -108,54 +114,108 @@ public class PaintMain extends JFrame{
         }
 
         @Override
-        public void mouseMoved(MouseEvent e) {
+        public void mouseMoved(MouseEvent e) {}
+    };
 
+    MouseMotionListener drawPenFirstListener = new MouseMotionListener() {
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            penStartX = e.getX();
+            penStartY = e.getY();
         }
-    }
 
-    public class drawButtonClickListener implements ActionListener{
+        @Override
+        public void mouseMoved(MouseEvent e) {}
+    };
+
+    MouseListener drawShapeListener = new MouseListener() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            switch(shapeType){
+                case 0:
+                    Circle circle = new Circle();
+                    circle.x1 = e.getX();
+                    circle.y1 = e.getY();
+                    circle.color = Color.black;
+                    circle.Draw(graphics2D);
+                    break;
+                case 1:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+            panelDraw.removeMouseListener(drawShapeListener);
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {}
+
+        @Override
+        public void mouseReleased(MouseEvent e) {}
+
+        @Override
+        public void mouseEntered(MouseEvent e) {}
+
+        @Override
+        public void mouseExited(MouseEvent e) {}
+    };
+
+    ActionListener drawButtonClickListener = new ActionListener(){
         @Override
         public void actionPerformed(ActionEvent e) {
             if(e.getSource() == btnCircle){
-
+                shapeType = 0;
+                panelDraw.removeMouseMotionListener(drawPenListener);
+                panelDraw.addMouseListener(drawShapeListener);
             }else if(e.getSource() == btnDiamond){
-
+                JOptionPane.showMessageDialog(null, "준비중입니다.", "", JOptionPane.WARNING_MESSAGE);
             }else if(e.getSource() == btnMouse){
-                panelDraw.addMouseMotionListener(new MouseMotionListener() {
-                    @Override
-                    public void mouseDragged(MouseEvent e) {
-                        penStartX = e.getX();
-                        penStartY = e.getY();
-                    }
-
-                    @Override
-                    public void mouseMoved(MouseEvent e) {
-
-                    }
-                });
-                panelDraw.addMouseMotionListener(new drawPen());
+                panelDraw.addMouseMotionListener(drawPenFirstListener);
+                panelDraw.addMouseMotionListener(drawPenListener);
             }else if(e.getSource() == btnRectangle){
-                
+                JOptionPane.showMessageDialog(null, "준비중입니다.", "", JOptionPane.WARNING_MESSAGE);
             }else if(e.getSource() == btnTriangle){
-
+                JOptionPane.showMessageDialog(null, "준비중입니다.", "", JOptionPane.WARNING_MESSAGE);
             }
         }
-    }
+    };
 
-    public class buttonClickListener implements ActionListener{
+    ActionListener buttonClickListener = new ActionListener(){
         @Override
         public void actionPerformed(ActionEvent e) {
             if(e.getSource() == btnColor){
-
+                JOptionPane.showMessageDialog(null, "준비중입니다.", "", JOptionPane.WARNING_MESSAGE);
             }else if(e.getSource() == btnJPEG){
-
+                JOptionPane.showMessageDialog(null, "준비중입니다.", "", JOptionPane.WARNING_MESSAGE);
             }else if(e.getSource() == btnMove){
-
+                JOptionPane.showMessageDialog(null, "준비중입니다.", "", JOptionPane.WARNING_MESSAGE);
             }else if(e.getSource() == btnRotate){
-
+                JOptionPane.showMessageDialog(null, "준비중입니다.", "", JOptionPane.WARNING_MESSAGE);
             }else if(e.getSource() == btnSize){
-
+                JOptionPane.showMessageDialog(null, "준비중입니다.", "", JOptionPane.WARNING_MESSAGE);
             }
+        }
+    };
+
+    public abstract class Shape implements Serializable{
+        Color color;
+        int x1, y1;
+
+        abstract void Draw(Graphics graphics);
+    }
+
+    public class Circle extends Shape{
+        Circle(){
+            super();
+        }
+
+        @Override
+        void Draw(Graphics graphics) {
+            graphics2D = (Graphics2D)graphics;
+            graphics2D.setColor(color);
+            graphics2D.drawOval(x1 - 50, y1 + 50, 100, 100);
         }
     }
 }
