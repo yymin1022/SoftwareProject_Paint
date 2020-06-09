898c15dfecff3d30e67e956da4ddbe73e9d2c0be
yymin1022 <yymin1022@gmail.com>
2020-06-09 16:44:06 +0900

    Update Main Class

--- src/PaintMain.java
+++ src/PaintMain.java
@@ -1,3 +1,4 @@
+import controller.PaintController;
 import model.Shape;
 import view.PaintView;
 
@@ -6,6 +7,8 @@ import java.util.ArrayList;
 public class PaintMain{
     public static void main(String[] arg0){
         ArrayList<Shape> shapeArrayList = new ArrayList<>();
+        
         PaintView paintView = new PaintView(shapeArrayList);
+        PaintController paintController = new PaintController(shapeArrayList, paintView);
     }
 }
