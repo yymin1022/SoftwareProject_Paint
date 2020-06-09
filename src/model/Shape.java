0a51a50ebff914b993e23a811fdf318ffe9aa802
yymin1022 <yymin1022@gmail.com>
2020-06-09 12:14:54 +0900

    Update Shape : Add Functions for get Shape Point Information

--- src/model/Shape.java
+++ src/model/Shape.java
@@ -11,6 +11,22 @@ public abstract class Shape implements Serializable {
 
     public Color color;
 
+    public double getStartX(){
+        return startX;
+    }
+
+    public double getStartY(){
+        return startY;
+    }
+
+    public double getEndX(){
+        return endX;
+    }
+
+    public double getEndY(){
+        return endY;
+    }
+
     public void setPoints(double startX, double startY, double endX, double endY){
         this.startX = startX;
         this.startY = startY;
