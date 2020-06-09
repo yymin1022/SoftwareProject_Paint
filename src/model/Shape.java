8c6d362153aa782873f5fd486703fbfc69e5292a
yymin1022 <yymin1022@gmail.com>
2020-06-09 12:13:13 +0900

    Update Shape : Add Functions for Drawing

--- src/model/Shape.java
+++ src/model/Shape.java
@@ -11,5 +11,28 @@ public abstract class Shape implements Serializable {
 
     public Color color;
 
+    public void setPoints(double startX, double startY, double endX, double endY){
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+
+        this.width = (int)Math.abs(endX - startX);
+        this.height = (int)Math.abs(endY - startY);
+    }
+
+    public void setStartXY(double startX, double startY){
+        this.startX = startX;
+        this.startY = startY;
+    }
+
+    public void setEndXY(double endX, double endY){
+        this.endX = endX;
+        this.endY = endY;
+
+        this.width =(int)Math.abs(endX - startX);
+        this.height = (int)Math.abs(endY - startY);
+    }
+
     abstract void Draw(Graphics graphics);
 }
