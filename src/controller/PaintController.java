6289d15f2ccc7657b6e72e288495327b8a6496e6
yymin1022 <yymin1022@gmail.com>
2020-06-09 16:24:38 +0900

    Update Controller : Initialized Code

--- src/controller/PaintController.java
+++ src/controller/PaintController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import model.Shape;
+import view.PaintView;
+
+import java.util.ArrayList;
+
 public class PaintController {
+    public double startX;
+    public double startY;
+    public double endX;
+    public double endY;
+
+    public ArrayList<Shape> shapeList;
+    public PaintView paintView;
+
+    public PaintController(ArrayList<Shape> shapeList, PaintView paintView){
+        this.paintView = paintView;
+        this.shapeList = shapeList;
+    }
 }
