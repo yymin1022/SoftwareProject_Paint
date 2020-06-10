6855604d672e54c57ff69fdfc51b2724a0ff1071
yymin1022 <yymin1022@gmail.com>
2020-06-10 13:43:49 +0900

    Update Controller : Add Mouse Listeners

--- src/PaintMain.java
+++ src/PaintMain.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class PaintMain{
     public static void main(String[] arg0){
         ArrayList<Shape> shapeArrayList = new ArrayList<>();
-        
+
         PaintView paintView = new PaintView(shapeArrayList);
         PaintController paintController = new PaintController(shapeArrayList, paintView);
     }
--- src/controller/PaintController.java
+++ src/controller/PaintController.java
@@ -4,6 +4,10 @@ import model.Shape;
 import view.PaintPanel;
 import view.PaintView;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 public class PaintController{
@@ -15,10 +19,48 @@ public class PaintController{
     public ArrayList<Shape> shapeArrayList;
     public PaintPanel paintPanel;
     public PaintView paintView;
+    public Shape selectedShape;
 
     public PaintController(ArrayList<Shape> shapeArrayList, PaintView paintView){
         this.paintPanel = paintView.getPaintPanel();
         this.paintView = paintView;
         this.shapeArrayList = shapeArrayList;
+    }
+
+    public class panelMouseListener implements MouseListener, MouseMotionListener{
+        @Override
+        public void mouseClicked(MouseEvent e){
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e){
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e){
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e){
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e){
+
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e){
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e){
+
+        }
     }
 }
