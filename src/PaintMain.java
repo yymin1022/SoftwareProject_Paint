36b9548b053cd4fcf4011f15c223a217fdc7dfc3
yymin1022 <yymin1022@gmail.com>
2020-06-14 00:40:28 +0900

    Improved PaintMain Class Runtime

--- src/PaintMain.java
+++ src/PaintMain.java
@@ -2,13 +2,14 @@ import controller.PaintController;
 import model.Shape;
 import view.PaintView;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class PaintMain{
     public static void main(String[] arg0){
         ArrayList<Shape> shapeArrayList = new ArrayList<>();
-
         PaintView paintView = new PaintView(shapeArrayList);
-        PaintController paintController = new PaintController(shapeArrayList, paintView);
+
+        new PaintController(shapeArrayList, paintView);
     }
 }
